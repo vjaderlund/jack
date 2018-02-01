718d6bfc40835bf36ae0cb8051d4f4aa2838315a
Unknown <viktor.jaderlund@medieinstitutet.se>
2018-02-01 11:49:59 +0100

    Updated rest api

--- js/wp_rest.js
+++ js/wp_rest.js
@@ -1,1 +1,28 @@
 //A REST API FROM WORDPRESS
+(function($) {
+  $(document).ready(function() {
+    var url = "http://localhost/kingkong/wp-json/wp/v2/posts/?_embed=true";
+
+    $.ajax({
+      type: "GET",
+      url: url,
+      timeout: 2000,
+      beforeSend: function() {
+        console.log('before');
+      },
+      complete: function() {
+        console.log('COMPLETE');
+      },
+      success: function(myData) {
+        console.log(myData);
+      },
+      error: function() {
+        console.log('ERROR');
+      }
+    })
+
+
+
+
+  });
+})(jQuery)
