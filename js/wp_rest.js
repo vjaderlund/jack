099cffce9cefa9d59a588e8aaabed336ca146d32
Unknown <viktor.jaderlund@medieinstitutet.se>
2018-03-13 10:49:17 +0100

    Update

--- js/wp_rest.js
+++ js/wp_rest.js
@@ -1,45 +1,53 @@
 //A REST API FROM WORDPRESS
 (function($) {
-  $(document).ready(function() {
-    // var url = "http://localhost/kingkong/wp-json/wp/v2/posts";
-    var url = "http://localhost/kingkong/wp-json/wp/v2/posts/?_embed=true";
+    $(document).ready(function() {
+        var url = "http://localhost/kingkong/wp-json/wp/v2/posts/?_embed=true";
 
-    $.ajax({
-      type: "GET",
-      url: url,
-      timeout: 2000,
-      beforeSend: function() {
-        console.log('before');
-      },
-      complete: function() {
-        console.log('COMPLETE');
-      },
-      success: function(myData) {
-        displayWP(myData);
-        console.log(myData);
-      },
-      error: function() {
-        console.log('ERROR');
-      }
-    });
+        $.ajax({
+          type: "GET",
+          url: url,
+          timeout: 2000,
+          beforeSend: function() {
+            console.log('before');
+          },
+          complete: function() {
+            console.log('COMPLETE');
+          },
+          success: function(myData) {
+            displayWP(myData);
+            console.log(myData);
+          },
+          error: function() {
+            console.log('ERROR');
+          }
+        });
 
-    function displayWP(myData) {
+        function displayWP(myData) {
+          for (var i = 0; i < myData.length; i++) {
+            var wpTitle = myData[i].title.rendered;
+            var wpContent = myData[i].content.rendered;
+            // console.log(myData[i].content.rendered);
+            if (myData[i]._embedded['wp:featuredmedia']) {
+              var wpPicture = myData[i]._embedded['wp:featuredmedia'];
+              for (var i2 = 0; i < wpPicture.length; i2++) {
+                var wpPicture[i2]
 
-      for (var i = 0; i < myData.length; i++) {
-        var wpTitle = myData[i].title.rendered;
-        var wpContent = myData[i].content.rendered;
-        // console.log(myData[i].content.rendered);
-        if (myData[i]._embedded['wp:featuredmedia']) {
-          var wpPicture = myData[i]._embedded['wp:featuredmedia'];
-          // for (var i2 = 0; i < wpPicture.length; i2++) {
-          // var wpPicture[i2]
-          // }
-        }
-      }
-    }
+                //Title
+                //Picture
+                //content
+                var wpHTML = ' ';
+                wpHTML += '<section>';
+                wpHTML += '<h1>' + wpTitle + '</h1>';
+
+                wpHTML += '</section>';
+                $('body').append(wpHTML;
+                }
+              }
+            }
+          }
 
 
 
 
-  });
-})(jQuery)
+        });
+    })(jQuery)
